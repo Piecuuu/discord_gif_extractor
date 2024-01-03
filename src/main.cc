1800d8189da51b2a72b5619c13c352d014e311a6
Piecuuu <56731916+Piecuuu@users.noreply.github.com>
2024-01-03 14:51:24 +0100

    Added argument support

--- src/main.cc
+++ src/main.cc
@@ -5,6 +5,7 @@
 #include <google/protobuf/util/json_util.h>
 #include <nlohmann/json.hpp>
 #include <fstream>
+#include <argparse/argparse.hpp>
 
 std::string protobuf_to_json(const std::string& protobuf_data) {
   auto const decoded_data = base64pp::decode(protobuf_data);
@@ -32,21 +33,66 @@ std::vector<std::string> parse_json(const std::string& json_str) {
   return links;
 }
 
-int main() {
+void write_links_to_stdout(std::vector<std::string> links) {
+  for(auto& link : links) {
+    std::cout << link << "\n";
+  }
+}
+
+void write_links_to_file(std::vector<std::string> links, std::string linksfile_path) {
+  std::ofstream linksfile(linksfile_path);
+  for(auto& link : links) {
+    linksfile << link << "\n";
+  }
+  linksfile.close();
+}
+
+int main(int argc, char *argv[]) {
+  argparse::ArgumentParser program("discord_gif_extractor");
+  program.add_argument("-o", "--output")
+    .help("File to output to")
+    .default_value(std::string("links.txt"));
+  program.add_argument("-i", "--input")
+    .help("File to get data from")
+    .default_value(std::string("data.txt"));
+  program.add_argument("-d", "--decode-only")
+    .help("Only decode protobuf and output JSON data")
+    .flag();
+
+  try {
+    program.parse_args(argc, argv);    // Example: ./main --color red --color green --color blue
+  }
+  catch (const std::exception& err) {
+    std::cerr << err.what() << std::endl;
+    std::cerr << program;
+    return 1;
+  }
+
+  std::string outputfile_path = program.get<std::string>("--output");
+  std::string datafile_path = program.get<std::string>("--input");
+  bool write_to_stdout = outputfile_path == "-";
+
   std::string data;
   //std::cin >> data;
-  std::ifstream datafile("data.txt");
+  std::ifstream datafile(datafile_path);
   std::getline(datafile, data);
 
   std::string json_data = protobuf_to_json(data);
   datafile.close();
 
-  std::vector<std::string> links = parse_json(json_data);
-  std::ofstream linksfile("links.txt");
-  for(auto& link : links) {
-    linksfile << link << "\n";
+  if(program["--decode-only"] == true) {
+    if(write_to_stdout) {
+      std::cout << json_data << "\n";
+      return 0;
+    }
+    std::ofstream outputfile(outputfile_path);
+    outputfile << json_data << "\n";
+    outputfile.close();
+    return 0;
   }
-  linksfile.close();
+
+  std::vector<std::string> links = parse_json(json_data);
+  write_to_stdout ? write_links_to_stdout(links) : write_links_to_file(links, outputfile_path);
 
   //std::cout << json_data << "\n";
   return 0;
